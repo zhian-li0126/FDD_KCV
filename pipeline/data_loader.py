be9acf508bd5b48a9729b757c2ebd4c6eb3e545b
zhian-li0126 <zanli@ucdavis.edu>
2025-02-08 07:15:08 -0800

    Change the commit message in the editor

--- pipeline/data_loader.py
+++ pipeline/data_loader.py
@@ -7,10 +7,19 @@ def load_data(model_name):
     Loads the dataset based on model type.
     """
     if model_name.startswith("pH_"):
-        x, y = load_prepare_diagnosis(split=False)
+        x, y = load_prepare_diagnosis()
     elif model_name.startswith("EC_"):
         x, y = load_prepare_diagnosis(split=False)
     else:
         raise ValueError(f"Unknown model type: {model_name}")
 
     return x, y
+
+if __name__ == "__main__":
+    import os
+    print("Current Working Directory:", os.getcwd())
+    try:
+        x, y = load_data("pH_LSTM")
+
+    except:
+        pass
